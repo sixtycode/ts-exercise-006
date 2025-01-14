a7a249312eb023bfbca91af52475b8ad2ce6b250
sixtycode <sagaraperkasa.work@gmail.com>
2025-01-14 07:33:30 +0700

    feat: exercise six initialization

---
+++ tsSix.ts
@@ -0,0 +1,34 @@
+// Exercise 1
+// ● Create a function to calculate array of student data
+// ● The object has this following properties :
+//  ○ Name → String
+//  ○ Email → String
+//  ○ Age → Date
+//  ○ Score → Number
+// ● Parameters : array of student
+// ● Return values :
+//  ○ Object with this following properties :
+//      ■ Score
+//          ● Highest
+//          ● Lowest
+//          ● Average
+//      ■ Age
+//          ● Highest
+//          ● Lowest
+//          ● Average
+
+// Exercise 2
+// ● Create a program to create transaction
+// ● Product Class
+//  ○ Properties
+//      ■ Name
+//      ■ Price
+// ● Transaction Class
+//  ○ Properties
+//      ■ Total
+//      ■ Product
+//          ● All product data
+//          ● Qty
+// ○ Add to cart method → Add product to transaction
+// ○ Show total method → Show total current transaction
+// ○ Checkout method → Finalize transaction, return transaction data
